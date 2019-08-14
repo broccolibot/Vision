424e8853868f1e49066408c993466612e483e84e
Masterchef365 <duncan.freeman1@gmail.com>
2019-08-13 20:54:04 -0700

    Simple config save/load

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,10 @@
 #include <pipeline.hpp>
 #include <pipeline_ui.hpp>
 
-int main() {
+int main(int argc, char** argv) {
+    char* config_dir = (char*)"config.yml";
+    if (argc > 1) config_dir = argv[0];
+
     cv::Mat bgr_frame =
         cv::imread("/home/duncan/Media/Images/datasets/Rec1/bgr-1532.png");
     cv::Mat depth_frame =
@@ -12,18 +15,49 @@ int main() {
     assert(depth_frame.type() == CV_16UC1);
 
     BroccoliDetector detector;
+    
+    // Set defaults
     detector.area_threshold = 2;
     detector.laplacian_threshold = 30;
     detector.morph_size = 5;
     detector.record_hsv_thresh = true;
     detector.record_laplacian = false;
     detector.record_morph_blob = false;
+    int hist_min = 10;
+    int hist_max = 4000;
+    float percentile = 0.1;
+    {
+        cv::FileStorage fs(config_dir, cv::FileStorage::FORMAT_YAML | cv::FileStorage::READ);
+        if (fs.isOpened()) {
+            fs["area_threshold"] >> detector.area_threshold;
+            fs["laplacian_threshold"] >> detector.laplacian_threshold;
+            fs["morph_size"] >> detector.morph_size;
+            fs["hist_min"] >> hist_min;
+            fs["hist_max"] >> hist_max;
+            fs["percentile"] >> percentile;
+            fs.release();
+        }
+    }
 
-    auto pipeline = std::make_shared<Pipeline>(std::move(detector), 10, 4000, 0.1);
+    auto pipeline = std::make_shared<Pipeline>(std::move(detector), hist_min, hist_max, percentile);
+
     PipelineUI ui(pipeline);
 
     while (cv::waitKey(20) != 'q') {
         auto output = pipeline->next_frameset(bgr_frame, depth_frame);
         ui.next_frame(bgr_frame, output);
+    }
+
+    {
+        cv::FileStorage fs(config_dir, cv::FileStorage::FORMAT_YAML | cv::FileStorage::WRITE);
+        if (fs.isOpened()) {
+            fs << "area_threshold" << pipeline->detector.area_threshold;
+            fs << "laplacian_threshold" << pipeline->detector.laplacian_threshold;
+            fs << "morph_size" << pipeline->detector.morph_size;
+            fs << "hist_min" << hist_min;
+            fs << "hist_max" << hist_max;
+            fs << "percentile" << pipeline->percentile;
+            fs.release();
+        }
     }
 }
